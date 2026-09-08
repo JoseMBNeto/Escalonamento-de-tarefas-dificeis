3f9a848e202b223cbacf2fee93f4493db9052445
JoseMBNeto <miltoneto4231@gmail.com>
2026-09-07 21:05:35 -0300

    Adicionando funções para ver qual é a tarefa de maior prioridade e quando ocorre a perda de deadline

--- tarefa.c
+++ tarefa.c
@@ -1,4 +1,5 @@
 #include "tarefa.h"
+#include <string.h>
 
 void inicializarSimulacao (Tarefa *tarefas, int quantidade){
     for (int i = 0; i < quantidade; i++){
@@ -24,4 +25,40 @@ void novaInstancia(Tarefa *tarefa, long instanteAtual){
     tarefa->deadlineAbs = instanteAtual + tarefa->deadline;
     tarefa->burstRestante = tarefa->burst;
     tarefa->ativa = 1;
+}
+
+int escolherProximaTarefa(Tarefa *tarefas, int quantidade, const char *algoritmo){
+    int indice = -1;
+
+    for (int i = 0; i < quantidade; i++){
+        if(!tarefas[i].ativa){
+            continue;
+        }
+
+        if (indice == -1){
+            indice = i;
+            continue;
+        }
+
+        if (strcmp(algoritmo, "rate") == 0){
+            if (tarefas[i].periodo < tarefas[indice].periodo){
+                indice = i;
+            }
+        }else{
+            if (tarefas[i].deadlineAbs < tarefas[indice].deadlineAbs){
+                indice = i;
+            }
+        }
+    }
+
+    return indice;
+}
+
+void detectarPerdaDeadline (Tarefa *tarefas, int quantidade, long instanteAtual){
+    for (int i = 0; i < quantidade; i++){
+        if (tarefas[i].ativa && instanteAtual >= tarefas[i].deadlineAbs);{
+            tarefas[i].perdidas++;
+            tarefas[i].ativa = 0;
+        }
+    }
 }
